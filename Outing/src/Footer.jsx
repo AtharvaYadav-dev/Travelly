11920210c7dd6f4c8effc4cfb49f05e871530262
Atharva Yadav <121742156+AtharvaYadav-dev@users.noreply.github.com>
2025-09-16 22:35:15 +0530

    Update Footer.jsx

--- Outing/src/Footer.jsx
+++ Outing/src/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="flex gap-4 text-sm">
           <a href="https://www.instagram.com/accounts/login/?hl=en" target="_blank" className="hover:text-indigo-100 transition">Instagram</a>
           <a href="https://x.com/i/flow/login?lang=en" target="_blank" className="hover:text-indigo-100 transition">Twitter</a>
-          <a href="https://travelly1-seven.vercel.app/contact" target="_blank" className="hover:text-indigo-100 transition">Contact</a>
+          <a href="/contact" target="_blank" className="hover:text-indigo-100 transition">Contact</a>
         </div>
       </div>
     </footer>
