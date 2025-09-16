e4e69ee900d5cbf21a59b8d800f8b854fa09759b
Atharva Yadav <121742156+AtharvaYadav-dev@users.noreply.github.com>
2025-09-16 22:21:56 +0530

    Update Footer.jsx

--- Outing/src/Footer.jsx
+++ Outing/src/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 flex flex-col sm:flex-row items-center justify-between gap-3">
         <div className="font-bold text-lg">Travelly © 2025</div>
         <div className="flex gap-4 text-sm">
-          <a href="https://www.instagram.com/accounts/login/?hl=en" className="hover:text-indigo-100 transition">Instagram</a>
-          <a href="https://x.com/i/flow/login?lang=en" className="hover:text-indigo-100 transition">Twitter</a>
-          <a href="https://travelly1-seven.vercel.app/contact" className="hover:text-indigo-100 transition">Contact</a>
+          <a href="https://www.instagram.com/accounts/login/?hl=en" target="_blank" className="hover:text-indigo-100 transition">Instagram</a>
+          <a href="https://x.com/i/flow/login?lang=en" target="_blank" className="hover:text-indigo-100 transition">Twitter</a>
+          <a href="https://travelly1-seven.vercel.app/contact" target="_blank" className="hover:text-indigo-100 transition">Contact</a>
         </div>
       </div>
     </footer>
